d3a095fa1091b87c9c15b66ebd65095a17a98215
parkmyungwan <mywanpark@gmail.com>
2023-01-15 18:52:12 +0900

    Basic: Type Assertion but not recommended

--- 1-basic/primitive-type.ts
+++ 1-basic/primitive-type.ts
@@ -50,4 +50,97 @@
 
   type Foods = 'banana';
   const fruit1: Foods = 'banana';
+
+  type SuccessState = {
+    result: 'success';
+    response: {
+      body: string;
+    };
+  };
+
+  type FailureState = {
+    result: 'fail';
+    reason: string;
+  };
+
+  type LoginState = SuccessState | FailureState;
+
+  function login(loginState: LoginState): void {
+    if (loginState.result === 'success') {
+      console.log('success');
+      return;
+    }
+    if (loginState.result === 'fail') {
+      console.log('fail');
+      return;
+    }
+  }
+
+  type Obj1 = {
+    name: string;
+  };
+
+  const obj1: Obj1 = {
+    name: 'park',
+  };
+
+  if ('name' in obj1) {
+    console.log('key is here!');
+  }
+
+  login({
+    result: 'fail',
+    reason: 'network error',
+  });
+
+  type UnivStudent = {
+    name: string;
+    score: number;
+  };
+
+  type Worker = {
+    employeeId: string;
+    work: () => void;
+  };
+
+  function work(person: UnivStudent & Worker) {
+    console.log(person.employeeId, person.work(), person.score);
+  }
+
+  work({
+    name: 'park',
+    score: 100,
+    employeeId: '123',
+    work: () => {},
+  });
+
+  const days = Object.freeze({
+    Monday: 'mon',
+    Tuesday: 'tu',
+    Wednesday: 'wed',
+  });
+
+  enum Days {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+  }
+
+  type DaysOfWeek = 'Mon' | 'Tuesday' | 'Wednesday';
+  let day: DaysOfWeek = 'Mon';
+  let date: Days = 10;
+
+  let text1 = 'hello';
+
+  function add(a: number, b: number): number {
+    return a + b;
+  }
+  let result = add(1, 2);
+
+  function findNumber(): Array<number> | undefined {
+    return undefined;
+  }
+
+  let numArr = findNumber()!.length;
 }
