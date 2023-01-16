8d7b9ab09f754fb2942dff5ea054b0524a4748ee
parkmyungwan <mywanpark@gmail.com>
2023-01-16 15:17:05 +0900

    Basic: Getter and Setter for reuse

--- CoffeeMaker.ts
+++ CoffeeMaker.ts
@@ -5,20 +5,23 @@
   };
 
   class CoffeeMaker {
-    static GRAMS_NEEDED_PER_SHOT = 18;
-    coffee_powder: number;
+    private static GRAMS_NEEDED_PER_SHOT = 18;
+    private coffee_powder: number = 0;
 
-    constructor(coffee_powder: number) {
-      this.coffee_powder = coffee_powder;
+    static makeMachine(): CoffeeMaker {
+      return new CoffeeMaker();
     }
 
-    static makeMachine(coffee_powder: number): CoffeeMaker {
-      return new CoffeeMaker(coffee_powder);
+    public fillCoffee(beans: number): void {
+      if (beans < 0) {
+        throw new Error('beans must be over zero');
+      }
+      this.coffee_powder = beans;
     }
 
-    makeCoffee(coffee_powder: number): CoffeeCup {
+    makeCoffee(): CoffeeCup {
       let coffee_cup: number =
-        coffee_powder / CoffeeMaker.GRAMS_NEEDED_PER_SHOT;
+        this.coffee_powder / CoffeeMaker.GRAMS_NEEDED_PER_SHOT;
 
       return {
         cups: coffee_cup,
@@ -26,7 +29,30 @@
       };
     }
   }
-  const coffeeMaker: CoffeeMaker = new CoffeeMaker(50);
-  const coffeeMaker2 = CoffeeMaker.makeMachine(40);
-  console.log(coffeeMaker2);
+  const maker = CoffeeMaker.makeMachine();
+  // maker.fillCoffee(-1);
+  // console.log(maker.makeCoffee());
+
+  class User {
+    private userAge: number = 0;
+
+    constructor(private firstName: string, private lastName: string) {}
+
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+
+    set age(age: number) {
+      this.userAge = age;
+    }
+
+    get age(): number {
+      return this.userAge;
+    }
+  }
+  const user1 = new User('park', 'myungwan');
+  console.log(user1.fullName);
+  console.log(user1.age);
+  user1.age = 5;
+  console.log(user1.age);
 }
